d85ccda004d0179a9951894ab559f11070b29dc9
Shivika <shivika.jindal01@gmail.com>
2022-02-24 20:38:29 -0800

    to unfold knarly json

--- json-placeholder.py
+++ json-placeholder.py
@@ -2,14 +2,13 @@ import json
 import requests
 
 response = requests.get("https://jsonplaceholder.typicode.com/todos")
-print(response)
 
 jsonData = json.loads(response.text)
+#print(json.dumps(jsonData, indent=4, sort_keys=True))
 
 def checkTodo(json_data):
 
-  #if json_data["completed"] == "false":
-  check = json_data["completed"] 
+  check = json_data["completed"]
   userID = json_data["userId"]
   return check and userID
 
--- rest-api-1.py
+++ rest-api-1.py
@@ -1,12 +1,16 @@
 from getpass import getpass
 import requests
+import json
 
 response = requests.get("https://api.github.com/search/repositories")
 doc = response.json()
+#doc1 = json.loads(response.text)
+#doc1 = json.dumps(doc1, indent=4)
+#print(doc1)
 print(doc['documentation_url'])
 print(doc['errors'][0]['code'])
 
-res = requests.get('https://api.github.com/user', auth=('username', getpass()))
+#res = requests.get('https://api.github.com/user', auth=('username', getpass()))
 
 '''
 O/P:
